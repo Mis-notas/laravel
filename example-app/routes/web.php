9c7697988573e5ae9a6c4b17c8a92fb511bca170
Oscar Sabogal <oscarsabogal285@gmail.com>
2022-07-06 16:52:20 -0500

    agreed commit

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -18,17 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::resource('client', ClientController::class); // Se crea de forma automatica varias rutas para aceder a los diferentes metodos de la clase
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
-
 Route::get('/hola/{name}', [HolaController::class, 'index' ]);
 Route::get('categoria', [CategoryController::class, 'getCategoria']);
 Route::get('categoria/{id}', [CategoryController::class, 'getCategoriaId']);
 Route::post('/addCategoria', [CategoryController::class, 'insertCategoria']);
 Route::put('/updateCategoria/{id}', [CategoryController::class, 'updateCategoria']);
 Route::delete('deleteCategoria/{id}', [CategoryController::class, 'deleteCategoria']);
-
 require __DIR__.'/auth.php';
